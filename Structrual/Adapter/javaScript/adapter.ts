041b2683caf4b8d0066c878e229c1e5bb004d992
crypto <donischle75565@gmail.de>
2025-08-29 15:51:01 +0200

    chore(adapter): adjust comment

--- Structrual/Adapter/javaScript/adapter.ts
+++ Structrual/Adapter/javaScript/adapter.ts
@@ -4,7 +4,7 @@ export class SquarePegAdapter extends RoundPeg {
   peg: SquarePeg;
 
   constructor(squarePeg: SquarePeg) {
-    super(0); // you dont need roundPeg radius
+    super(0); // you dont need roundPeg radius -> you could define a common interface to avoid this
     this.peg = squarePeg;
   }
 
